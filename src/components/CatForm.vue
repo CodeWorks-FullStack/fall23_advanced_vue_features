a4fcbe9f77982aa1d37db210227c1ddff62f35a7
Jeremy <jembyfowler@gmail.com>
2023-11-16 15:24:15 -0700

    student handoff

---
+++ src/components/CatForm.vue
@@ -0,0 +1,24 @@
+<template>
+  <form @submit.prevent="createCat">
+    <input type="text">
+    <button type="submit">Submit</button>
+  </form>
+</template>
+
+
+<script>
+import { logger } from '../utils/Logger.js';
+
+export default {
+  setup() {
+    return {
+      createCat() {
+        logger.log('CREATING CAT')
+      }
+    }
+  }
+}
+</script>
+
+
+<style lang="scss" scoped></style>
---
+++ src/pages/HomePage.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="container-fluid">
+    <section class="row">
+      <div class="col-12 text-center">
+        <button data-bs-toggle="modal" data-bs-target="#modalOne" class="btn btn-outline-info">
+          Open First Modal
+        </button>
+        <button data-bs-toggle="modal" data-bs-target="#modalTwo" class="btn btn-outline-info">
+          Open Second Modal
+        </button>
+        <button data-bs-toggle="modal" data-bs-target="#modalThree" class="btn btn-outline-info">
+          Open Second Modal
+        </button>
+      </div>
+    </section>
+  </div>
+
+  <ModalComponent :modalId="'modalOne'">
+    <template #modalTitle>
+      <b>Modal One</b>
+    </template>
+
+    <template #modalBody>
+      Sup doggies
+    </template>
+  </ModalComponent>
+
+  <ModalComponent :modalId="'modalTwo'" :modalSize="'modal-sm'" :showFooter="true">
+    <template #modalTitle>
+      <i>Modal Two</i>
+    </template>
+
+    <template #modalBody>
+      <CatForm />
+    </template>
+  </ModalComponent>
+
+  <ModalComponent :modalId="'modalThree'" :modalSize="'modal-fullscreen'">
+    <template #modalTitle>
+      Modal Three
+    </template>
+
+  </ModalComponent>
+</template>
+
+<script>
+import CatForm from '../components/CatForm.vue';
+import ModalComponent from '../components/ModalComponent.vue';
+
+export default {
+  setup() {
+    return {};
+  },
+  components: { ModalComponent, CatForm }
+}
+</script>
+
+<style scoped lang="scss"></style>
